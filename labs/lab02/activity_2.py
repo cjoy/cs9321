963eaead63a78e06780997558d17a675d20d3ff0
Chris Joy <chrisjoydev@gmail.com>
2018-08-13 22:40:58 +1000

    Added activity 2

--- labs/lab02/activity_2.py
+++ labs/lab02/activity_2.py
@@ -5,8 +5,18 @@ def main():
     # Load the dataset into a dataframe
     dataframe = pandas.read_csv('./dataset.csv')
     # Replace the cell value of "Place of Publication" with
-    # "London" if it contains "London", and replace all '-' characters with space
-    
+    # "London" if it contains "London", and replace all '-' characters with space    
+    dataframe['Place of Publication'] = dataframe['Place of Publication'].apply(
+        lambda s: 'London' if 'London' in s else s.replace('-', ' ')
+    )
+    # Keep the first 4 digit number in "Date of Publication"
+    dataframe['Date of Publication'] = dataframe['Date of Publication'].str.extract(r'^(\d{4})')
+    # Convert "Date of Publication" cells to numbers
+    dataframe['Date of Publication'] = pandas.to_numeric(dataframe['Date of Publication'], downcast='unsigned')
+    # Replace NaN with 0 for the cells of "Date of Publication"
+    dataframe['Date of Publication'] = dataframe['Date of Publication'].fillna(0)
+    # Print the dataframe to see if the changes have been applied properly
+    print(dataframe['Date of Publication'])
 
 if __name__ == '__main__':
     main()
