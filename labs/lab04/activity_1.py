b5413f8887aa153c88c2da7557adc5520c0644af
Chris Joy <chrisjoydev@gmail.com>
2018-08-27 22:50:12 +1000

    Incremental progress on labs

--- labs/lab04/activity_1.py
+++ labs/lab04/activity_1.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+import pandas
+from flask import Flask
+from flask_restplus import Resource, Api
+from ...common.Books import load_dataframe
+
+# Initialise flask
+app = Flask(__name__)
+api = Api(app)
+
+# Initialise global dataframe
+dataframe = load_dataframe()
+
+@api.route('/hello')
+class HelloWorld(Resource):
+  def get(self):
+    return {'hello': 'world'}
+
+if __name__ == '__main__':
+  app.run(debug=True)
