15565a58f73c900239956cc14b99d5739ce8929d
Chris Joy <chrisjoydev@gmail.com>
2018-08-30 15:38:24 +1000

    Incremental progress on lab04

--- labs/lab04/activity_1.py
+++ labs/lab04/activity_1.py
@@ -2,19 +2,30 @@
 import pandas
 from flask import Flask
 from flask_restplus import Resource, Api
-from ...common.Books import load_dataframe
 
 # Initialise flask
 app = Flask(__name__)
 api = Api(app)
 
-# Initialise global dataframe
-dataframe = load_dataframe()
+# clean dataset
+def clean(dataframe):
+  dataframe['Place of Publication'] = dataframe['Place of Publication'].apply(
+      lambda s: 'London' if 'London' in s else s.replace('-', ' ')
+  )
+  dataframe['Date of Publication'] = dataframe['Date of Publication'].str.extract(r'^(\d{4})')
+  dataframe['Date of Publication'] = pandas.to_numeric(dataframe['Date of Publication'], downcast='unsigned')
+  dataframe['Date of Publication'] = dataframe['Date of Publication'].fillna(0)
+  dataframe.columns = dataframe.columns.str.replace(' ', '_')
+  dataframe.set_index('Identifier', inplace=True)
+  return dataframe
 
-@api.route('/hello')
-class HelloWorld(Resource):
-  def get(self):
-    return {'hello': 'world'}
+@api.route('/books/<int:id>')
+class Books(Resource):
+  def get(self, id):
+    if id not in dataframe.index:
+      return {'error': 'Book not found.'}, 404
+    return dataframe.loc[id].to_dict(), 200
 
 if __name__ == '__main__':
+  dataframe = clean(pandas.read_csv('../common/Books.csv'))
   app.run(debug=True)
---
+++ labs/lab04/activity_2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import pandas
+from flask import Flask
+from flask_restplus import Resource, Api
+from activity_1 import clean
+
+# Initialise flask
+app = Flask(__name__)
+api = Api(app)
+
+@api.route('/books/<int:id>')
+class Books(Resource):
+  def get(self, id):
+    if id not in dataframe.index:
+      return {'error': 'Book not found.'}, 404
+    return dataframe.loc[id].to_dict(), 200
+  def delete(self, id):
+    if id not in dataframe.index:
+      return {'error': 'Book not found.'}, 404
+
+if __name__ == '__main__':
+  dataframe = clean(pandas.read_csv('../common/Books.csv'))
+  app.run(debug=True)
