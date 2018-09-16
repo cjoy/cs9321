9694801c2404dedf706a5c75ad4395f871be07bd
Chris Joy <chrisjoydev@gmail.com>
2018-09-16 14:15:24 +1000

    Refined ass2

--- ass2/z5113243_ass_2.py
+++ ass2/z5113243_ass_2.py
@@ -22,14 +22,24 @@ DB_CONFIG = {
 db = None # initialised in main
 app = Flask(__name__)
 app.config.SWAGGER_UI_DOC_EXPANSION = 'list'
-api = Api(app,
-          title='Assignment 2 - COMP9321 - Chris Joy (z5113243)',
-          description='Data Service for World Bank Economic Indicators.')
+api = Api(
+  app,
+  title='Assignment 2 - COMP9321 - Chris Joy (z5113243)',
+  description='In this assignment, we\'re asked to develop ' \
+  'a Flask-Restplus data service that allows a client to ' \
+  'read and store some publicly available economic indicator ' \
+  'data for countries around the world, and allow the consumers ' \
+  'to access the data through a REST API.'
+)
 indicator_model = api.model(COLLECTION, {
-  'indicator_id': fields.String,
+  'indicator_id': fields.String(required=True,
+                                title='An Indicator ',
+                                description='http://api.worldbank.org/v2/indicators',
+                                example='NY.GDP.MKTP.CD'),
 })
 parser = api.parser()
-parser.add_argument('q', help='Query param. Expected format: top<k> / bottom<k>')
+parser.add_argument('q', help='Query param. Expected format: top<k> / bottom<k>, ' \
+                              'where k is between 1 and 100. Eg. top10, bottom40')
 
 #------------- HELPER FUNCTIONS -------------#
 
@@ -38,9 +48,9 @@ def mlab_client(dbuser, dbpassword, mlab_inst, dbname):
     f'mongodb://{dbuser}:{dbpassword}@{mlab_inst}.mlab.com:39071/{dbname}'
   )[dbname]
 
-def api_url(indicator, date='2012:2017', format='json', page=1):
+def api_url(indicator, date='2012:2017', fmt='json', page=1):
   return 'http://api.worldbank.org/v2/countries/all/indicators/' \
-          f'{indicator}?date={date}&format={format}&page={page}'
+          f'{indicator}?date={date}&format={fmt}&page={page}'
 
 # Recursively build an array containing indicator data
 def get_indicator_data(indicator, page=1, prevRes=[], max_pages=MAX_PAGE_LIMIT):
@@ -57,7 +67,7 @@ def get_indicator_data(indicator, page=1, prevRes=[], max_pages=MAX_PAGE_LIMIT):
   )
 
 # Restructure indicator entry according to spec
-def format_indicator_entry(indicator_data):
+def format_collection_entry(indicator_data):
   return {
     'country': indicator_data['country']['value'],
     'date': indicator_data['date'],
@@ -102,32 +112,33 @@ class CollectionIndex(Resource):
         'creation_time': str(existing_collection['creation_time']),
         'indicator': existing_collection['indicator'],
       }, 200
-    # From now onwards we need to get data from the Worldbank API
+    # From now onwards we need to obtain data from the Worldbank API
     indicator_data = get_indicator_data(body['indicator_id'])
     # Valid indicator hasn't been specified (400)
     if indicator_data == 'Invalid indicator':
       return { 'message': 'Please specify a valid indicator.' }, 400
     # Create and retrieve indicator from Worldbank API (201)
-    indicator = {
+    collection = {
       'indicator': indicator_data[0]['indicator']['id'],
       'indicator_value': indicator_data[0]['indicator']['value'],
       'creation_time': datetime.datetime.utcnow(),
-      'entries': [format_indicator_entry(entry) for entry in indicator_data],
+      'entries': [format_collection_entry(entry) for entry in indicator_data],
     }
-    created_collection = db[COLLECTION].insert_one(indicator)
+    created_collection = db[COLLECTION].insert_one(collection)
     return {
       'location': f'/{COLLECTION}/{str(created_collection.inserted_id)}',
       'collection_id': str(created_collection.inserted_id),
-      'creation_time': str(indicator['creation_time']),
-      'indicator': indicator['indicator'],
+      'creation_time': str(collection['creation_time']),
+      'indicator': collection['indicator'],
     }, 201
 
   @api.doc(description='[Q3] Retrieve the list of available collections.')
   @api.response(200, 'Successfully retreieved collections.')
   @api.response(400, 'Unable to retreive collections.')
   def get(self):
-    collections = db[COLLECTION].find()
-    if not collections:
+    try:
+      collections = db[COLLECTION].find()
+    except:
       return { 'message': 'Unable to retrieve collections.' }, 400
     return [{
       'location': f'/{COLLECTION}/{str(doc["_id"])}',
@@ -136,8 +147,8 @@ class CollectionIndex(Resource):
       'indicator': doc['indicator'],
     } for doc in collections], 200
 
-@api.route(f'/{COLLECTION}/<collection_id>', endpoint=f'{COLLECTION}_byid')
-@api.param('collection_id', f'Unique id, distinguishing {COLLECTION}.')
+@api.route(f'/{COLLECTION}/<collection_id>', endpoint=f'{COLLECTION}_by_id')
+@api.param('collection_id', f'Unique id, used to distinguish {COLLECTION}.')
 class CollectionsById(Resource):
   @api.doc(description='[Q2] Deleting a collection with the data service.')
   @api.response(200, 'Successfully removed collection.')
@@ -145,17 +156,14 @@ class CollectionsById(Resource):
   @api.response(400, 'Unable to remove collection.')
   def delete(self, collection_id):
     # Check if collection exists
-    try:
-      db[COLLECTION].find_one({'_id': ObjectId(collection_id)})
-    except:
+    if not db[COLLECTION].find_one({'_id': ObjectId(collection_id)}):
       return { 'message': 'Unable to find collection.' }, 404
     # Remove collection from db
     try:
       db[COLLECTION].delete_one({'_id': ObjectId(collection_id)})
-      return { 'message': f'Collection = {collection_id} has been removed from the database!' }, 200
     except:
       return { 'message': 'Unable to remove collection.' }, 400
-    return { 'message': 'Unable to remove collection.' }, 400
+    return { 'message': f'Collection = {collection_id} has been removed from the database!' }, 200
 
   @api.doc(description='[Q4] Retrieve a collection.')
   @api.response(200, 'Successfully retreived collection.')
@@ -173,48 +181,49 @@ class CollectionsById(Resource):
       'entries': collection['entries'],
     }, 200
 
-@api.route(f'/{COLLECTION}/<collection_id>/<date>/<country>', endpoint=f'{COLLECTION}_countrydate')
-@api.param('collection_id', f'Unique id, distinguishing {COLLECTION}.')
-@api.param('date', 'Year (ranging from 2012 - 2017.)')
+@api.route(f'/{COLLECTION}/<collection_id>/<year>/<country>', endpoint=f'{COLLECTION}_countrydate')
+@api.param('collection_id', f'Unique id, used to distinguish {COLLECTION}.')
+@api.param('year', 'Year ranging from 2012 to 2017.')
 @api.param('country', 'Country identifier (eg. Arab World)')
 class CollectionByCountryYear(Resource):
-  @api.doc(description='[Q5] Retrieve economic indicator value for given country and a year.')
-  @api.response(200, 'Successfully retrieved economic indicator for given country.')
+  @api.doc(description='[Q5] Retrieve economic indicator value for given a country and year.')
+  @api.response(200, 'Successfully retrieved economic indicator for given a country and year.')
   @api.response(400, 'Unable to retrieve indicator entry.')
   @api.response(404, 'Unable to find collection.')
-  def get(self, collection_id, date, country):
+  def get(self, collection_id, year, country):
     try:
       collection = db[COLLECTION].find_one({'_id': ObjectId(collection_id)})
     except:
       return { 'message': 'Unable to find collection' }, 404
     # Create a filtered list containing entries that match params
     filtered_entries = [
-      entry for entry in collection['entries'] if entry['country'] == country and entry['date'] == date
+      entry for entry in collection['entries'] if entry['country'] == country and entry['date'] == year
     ]
     if len(filtered_entries) == 0:
-      return { 'message': f'Unable to find specific indicator entry.' }, 400
+      return {'message': 'Unable to find specific indicator entry ' \
+              f'for country=\'{country}\' and year=\'{year}\'.'}, 400
     return {
       'collection_id': str(collection['_id']),
       'indicator': collection['indicator'],
       **filtered_entries[0],
     }, 200
 
-@api.route(f'/{COLLECTION}/<collection_id>/<date>', endpoint=f'{COLLECTION}_topbottom')
-@api.param('collection_id', f'Unique id, distinguishing {COLLECTION}.')
-@api.param('date', 'Year (ranging from 2012 - 2017.)')
+@api.route(f'/{COLLECTION}/<collection_id>/<year>', endpoint=f'{COLLECTION}_by_top_bottom')
+@api.param('collection_id', f'Unique id, used to distinguish {COLLECTION}.')
+@api.param('year', 'Year ranging from 2012 to 2017.')
 class CollectionByTopBottom(Resource):
   @api.doc(description='[Q6] Retrieve top/bottom economic indicator values for a given year.')
   @api.response(200, 'Successfully retreived economic indicator values.')
   @api.response(404, 'Unable to find collection.')
   @api.expect(parser)
-  def get(self, collection_id, date):
+  def get(self, collection_id, year):
     query = request.args.get('q')
     try:
       collection = db[COLLECTION].find_one({'_id': ObjectId(collection_id)})
     except:
       return { 'message': 'Unable to find collection' }, 404
     filtered_entries = [
-      entry for entry in collection['entries'] if entry['date'] == date
+      entry for entry in collection['entries'] if entry['date'] == year
     ]
     if not query:
       return {
